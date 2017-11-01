6d69de1ceedce517a620be18f5b00c253b58fc34
xiyu <xiyu03@gmail.com>
2017-11-01 22:23:44 +0800

    update

---
+++ app/src/main/java/com/example/lance/simplebox/Adapter/ExpandableAdapter.java
@@ -0,0 +1,68 @@
+package com.example.lance.simplebox.Adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by xiyu0 on 2017/11/1.
+ */
+
+public class ExpandableAdapter extends BaseExpandableListAdapter {
+   private Activity activity;
+
+    public ExpandableAdapter(Activity a ){
+        activity=a;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return  childArray.get(groupPosition).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
--- app/src/main/java/com/example/lance/simplebox/View/DocumentBackUpActivity.java
+++ app/src/main/java/com/example/lance/simplebox/View/DocumentBackUpActivity.java
@@ -6,28 +6,55 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ExpandableListView;
 
 
+import com.example.lance.simplebox.Adapter.ExpandableAdapter;
 import com.example.lance.simplebox.Content.Content;
 import com.example.lance.simplebox.R;
 
 import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xiyu0 on 2017/10/31.
  */
 
 public class DocumentBackUpActivity extends AppCompatActivity{
-
+    private List<String> groupArray;
+    private  List<List<String>> childArray;
     private Toolbar toolbar;
+    private  ExpandableListView expandableListView ;
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.document_back_up);
+        initWidget();
+        initData();
+    }
 
-        toolbar= (Toolbar) findViewById(R.id.toolbar);
+    public void initWidget() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.font));
         toolbar.setTitle("文档备份");
+        expandableListView = (ExpandableListView)findViewById(R.id.expandableListView);
+        expandableListView.setAdapter(new ExpandableAdapter(DocumentBackUpActivity.this ));
     }
+
+     public void initData(){
+        groupArray = new  ArrayList<String>();    
+        childArray = new  ArrayList<List<String>>();  
+        groupArray.add("第一行" );
+        groupArray.add("第二行" );  
+        List<String> tempArray = new  ArrayList<String>();
+        tempArray.add("第一条" );  
+        tempArray.add("第二条" );  
+        tempArray.add("第三条" );
+        for (int  index = 0 ; index <groupArray.size(); ++index)  
+        {  
+            childArray.add(tempArray);  
+        }
+     }
 }
